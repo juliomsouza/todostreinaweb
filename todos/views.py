80d53984ac5b2702d01f4a664267a16c5f892abe
Julio Souza <infoexpress2014@gmail.com>
2024-03-30 21:50:20 -0300

    Criado template base, criado funções de salvar e editar

--- todos/views.py
+++ todos/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, UpdateView
 from .models import Todo
 from django.urls import reverse_lazy
 
@@ -12,3 +12,9 @@ class TodoCreateView(CreateView):
     model = Todo
     fields = ["title", "deadline"]
     success_url = reverse_lazy("todo_list")
+
+
+class TodoUpdateView(UpdateView):
+    model = Todo
+    fields = ["title", "deadline"]
+    success_url = reverse_lazy('todo_list')
