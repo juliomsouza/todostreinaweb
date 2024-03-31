20cc404f94a0157c65488f7304d882d9e8fd0563
Julio Souza <infoexpress2014@gmail.com>
2024-03-31 00:03:55 -0300

    Criado funções de bloquear a de edição e conclusão se a tarefa já estiver já concluída

--- todos/models.py
+++ todos/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from datetime import date
 
 
 class Todo(models.Model):
@@ -6,3 +7,11 @@ class Todo(models.Model):
     created_at = models.DateTimeField(auto_now_add=True, null=False, blank=False)
     deadline = models.DateField('Data para Terminar',null=False, blank=False)
     finished_at = models.DateField(null=True, blank=True)
+
+    class Meta:
+        ordering = ["deadline"]
+
+    def mark_as_complete(self):
+        if not self.finished_at:
+            self.finished_at = date.today()
+            self.save()
--- todos/views.py
+++ todos/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-from django.views.generic import ListView, CreateView, UpdateView
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView, View
 from .models import Todo
 from django.urls import reverse_lazy
+from django.shortcuts import get_object_or_404, redirect
 
 
 class TodoListView(ListView):
@@ -18,3 +19,17 @@ class TodoUpdateView(UpdateView):
     model = Todo
     fields = ["title", "deadline"]
     success_url = reverse_lazy('todo_list')
+
+
+class TodoDeleteView(DeleteView):
+    model = Todo
+    success_url = reverse_lazy('todo_list')
+
+
+class TodoCompleteView(View):
+    def get(self, request, pk):
+        todo = get_object_or_404(Todo, pk=pk)
+        #todo.finished_at = date.today()
+        #todo.save()
+        todo.mark_as_complete()
+        return redirect("todo_list")
